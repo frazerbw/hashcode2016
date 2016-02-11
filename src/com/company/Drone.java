cd9868e1fe420fb2435304f8a6d571a35ac26013
aca14tm <tmarshall4@sheffield.ac.uk>
2016-02-11 18:15:43 +0000

    initial commit!

---
+++ src/com/company/Drone.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by tomas on 11/02/16.
+ */
+public class Drone {
+
+}
---
+++ src/com/company/Point.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * Created by tomas on 11/02/16.
+ */
+public class Point {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+
+
+}
